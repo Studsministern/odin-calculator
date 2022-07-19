9a8732159164459eef1893992efa417536294ff1
Eric Weidow <eric@weidow.se>
2022-07-19 12:59:35 +0200

    Add button scripts
    Adds functionality for Clear, backspace, numbers, operators and equals.
    However, does NOT add functionality for decimal. Also doesn't support
    negative numbers and storing values of inputs.

--- js/calculator.js
+++ js/calculator.js
@@ -13,7 +13,7 @@ function operate(expression) {
         case '%':
             return modulu(numbers);
         default:
-            return 'ERROR: Operator not found';
+            return expression;
     }
 }
 
@@ -39,3 +39,66 @@ function divide(numberArray) {
 function modulu(numberArray) {
     return +numberArray[0] % +numberArray[1];
 }
+
+
+
+function lastCharOperator(expression) {
+    return containsOperator(expression.slice(-1));
+}
+
+function containsOperator(expression) {
+    return expression.match(/[+-/x%]/);
+}
+
+function removeLastChar(expression) {
+    return expression.slice(0, -1);
+}
+
+
+
+function clear() {
+    outputText.textContent = '0';
+}
+
+
+
+const outputText = document.querySelector('.output');
+const buttonClear = document.querySelector('#C');
+const buttonDecimal = document.querySelector('#decimal');
+const buttonBackspace = document.querySelector('#backspace');
+const buttonNumbers = document.querySelectorAll('.number');
+const buttonOperators = document.querySelectorAll('.operator');
+const buttonEquals = document.querySelector('#equals');
+
+buttonClear.addEventListener('click', () => clear());
+
+buttonDecimal.addEventListener('click', () => {
+    if (!outputText.textContent.match('.')) outputText.textContent += '.';
+});
+
+buttonBackspace.addEventListener('click', () => {
+    if(outputText.textContent !== 0) outputText.textContent = removeLastChar(outputText.textContent);
+    if(outputText.textContent === '') outputText.textContent = '0';
+});
+
+buttonNumbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if(outputText.textContent === '0') outputText.textContent = number.textContent;
+        else outputText.textContent += number.textContent;
+    });
+});
+
+buttonOperators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        const text = outputText.textContent;
+
+        if(lastCharOperator(text)) {
+            outputText.textContent = removeLastChar(text) + operator.textContent;
+        } else {
+            if(containsOperator(text)) outputText.textContent = operate(text);
+            outputText.textContent += operator.textContent;
+        }
+    });
+});
+
+buttonEquals.addEventListener('click', () => outputText.textContent = operate(outputText.textContent));
